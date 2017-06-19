81581be3505fa8553a2b1e66e0d910edd34c7b85
ltasneem <lubaba070@gmail.com>
2017-06-19 16:46:03 -0400

    upto what if activity

--- src/Boolean_decision.java
+++ src/Boolean_decision.java
@@ -88,6 +88,141 @@ public class Boolean_decision {
         else
             System.out.println("Enjoy your day!");
 		
+        /*What if debugging activity*/
+        
+        String fname, lname;
+		double gpa;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print( "Student's First Name? " );
+		fname = keyboard.next();
+		
+		System.out.print( "Student's Last Name? " );
+		lname = keyboard.next();
+
+		System.out.print( "Student's GPA? " );
+		gpa = keyboard.nextDouble();
+		
+		System.out.println();
+		System.out.println("Student Name :" + fname + " " + lname);
+		System.out.println("Student GPA :" + gpa);
+		if (gpa >= 3)
+		{
+			System.out.println("This student is in good standing.");
+		} else if (gpa == 1)
+		{
+			System.out.println("This student is on academic probation.");
+		} else
+		{
+			System.out.println("This student has been expelled.");
+		}
+		
+		
+		/*What if change activity*/
+		
+		String fnam, lnam, status;
+		double cgpa;
+		int hours;
+
+
+
+		System.out.print( "Student's First Name? " );
+		fnam = keyboard.next();
+
+		System.out.print( "Student's Last Name? " );
+		lnam = keyboard.next();
+
+		System.out.print( "Student's GPA? " );
+		cgpa = keyboard.nextDouble();
+
+		System.out.print( "Student's Current Course Load? " );
+		hours = keyboard.nextInt();
+
+		System.out.println();
+		if(hours >=12)
+		{
+			System.out.println("Student Name :" + fnam + " " + lnam);
+			System.out.println("Student GPA :" + cgpa);
+			System.out.println("Status :" + "Fulltime");
+			if (cgpa >= 3)
+			{
+			System.out.println("This student is in good standing.");
+			} else if (cgpa >= 2)
+			{
+			System.out.println("This student needs to study more.");
+			} else if (cgpa >= 1)
+			{
+			System.out.println("This student is on academic probation.");
+			} else
+			{
+			System.out.println("This student has been expelled.");
+			}
+		}
+		
+		
+		/*What if completion activity   sum, product and average
+		 *     If the calculated sum is over 200, print an asterisk next to the sum.*/
+		
+		int num1, num2;
+		System.out.print( "First Number? :" );
+		num1 = keyboard.nextInt();
+		
+		System.out.print( "Last Number?  :" );
+		num2 = keyboard.nextInt();
+		
+		System.out.println("sum : " + (num1+num2));
+		if((num1+num2)>200)
+		{
+			System.out.println("*");
+		}
+		System.out.println("product : " + (num1*num2));
+		System.out.println("average : " + (num1+num2)/2);
+		
+		/*What if activity
+		 * Customer ID: 101
+			Customer Name: Bart Simpson
+			Sales Amount: $500.00
+			Tax Code: NPF
+			Total Amount Due: $500.00
+			
+			tax code	tax amount
+			NRM	6%
+			NPF	0%
+			BIZ	4.5%
+
+		 	*/
+		
+		int id;
+		double sales_amount,total_amount;
+		String customer_name,tax_code;
+		
+		
+		System.out.println("What's your customer ID? : ");
+		id = keyboard.nextInt();
+		System.out.println("What's your name? : ");
+		customer_name = keyboard.next();
+		System.out.println("Please input your sales amount: ");
+		sales_amount = keyboard.nextDouble();
+		System.out.println("Please input your tax_codet: ");
+		tax_code = keyboard.next();
+		System.out.println(" Customer ID : " + id);
+		System.out.println(" Customer Name : " + customer_name);
+		System.out.println(" Sales Amount : " + sales_amount);
+		System.out.println(" Tax code : " + tax_code);
+		if(tax_code.equals("NPF"))
+		{
+			System.out.println("Total Amount Due: " +sales_amount);
+		}
+		else if(tax_code.equals("NRM"))
+		{
+			System.out.println("Total Amount Due: " +(sales_amount+sales_amount*.006));
+		}
+		else if(tax_code.equals("BIZ"))
+		{
+			System.out.println("Total Amount Due: " +(sales_amount+sales_amount*.045));
+		}
+		
 		
 	}
 
